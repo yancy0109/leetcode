490734fd08a4d5b259b3e590a44233c3fed9a422
yancy0109 <1156729237@qq.com>
2023-02-10 00:17:22 +0800

    复习q7

---
+++ src/main/java/leetcodeHot100/q7Reverse/App.java
@@ -0,0 +1,26 @@
+package leetcodeHot100.q7Reverse;
+
+/**
+ * @author yancy0109
+ */
+public class App {
+    public int reverse(int x) {
+        int result = 0;
+        while (x != 0) {
+            if (result < Integer.MIN_VALUE / 10 || result > Integer.MAX_VALUE / 10) {
+                return 0;
+            }
+            // result进位
+            result*=10;
+            // result + 当前x末尾
+            result+=x%10;
+            // x移除末位
+            x/=10;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().reverse(-2147483415));
+    }
+}
