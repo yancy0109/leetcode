9a86b07a0712ac5786dcc5860c139de04b15d21c
yancy0109 <1156729237@qq.com>
2023-03-03 18:04:56 +0800

    q7buildTree

---
+++ src/main/java/offersword/q7buildBinaryTree/Solution2.java
@@ -0,0 +1,19 @@
+package offersword.q7buildBinaryTree;
+
+/**
+ * 迭代法
+ * 哥 我看不懂，先溜了
+ * @author yancy0109
+ */
+public class Solution2 {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] preorder = {3, 9, 20, 15, 7};
+        int[] inorder = {9, 3, 15, 20, 7};
+        System.out.println(new Solution1().buildTree(preorder, inorder));
+    }
+}
